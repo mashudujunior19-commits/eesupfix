cff3afe222003952f9dae38c2c1890b6835b917a
MJPhalane <mjngomane09@gmail.com>
2024-11-25 19:31:22 +0200

    update

--- packages/ui/lib/app.dart
+++ packages/ui/lib/app.dart
@@ -38,7 +38,6 @@ import 'package:ui/src/views/finances/wallets/bloc/wallets_bloc.dart';
 import 'package:ui/src/views/notifications/bloc/notifications_bloc.dart';
 import 'package:ui/src/views/shop/browsing/bloc/products_filter_bloc.dart';
 import 'package:ui/src/views/shop/cart/bloc/cart_bloc.dart';
-import 'package:ui/src/views/shop/hampers/bloc/hamper_bloc.dart';
 
 class MainApp extends StatelessWidget {
   MainApp({super.key});
@@ -126,15 +125,6 @@ class MainApp extends StatelessWidget {
     ),
   );
 
-  final _hamperRepo = RepositoryProvider(
-    create: (context) => ShoppingRepository(
-      ShoppingSupabaseImp(
-        GetIt.I.get<SupabaseClient>(),
-      ),
-      context.read<AuthRepository>(),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
     ///get the correct theme based on the app type (eesup or my kasi)
@@ -154,8 +144,7 @@ class MainApp extends StatelessWidget {
         _paymentGatewayRepo,
         _partnerRepository,
         _surveysRepository,
-        _notificationRepo,
-        _hamperRepo
+        _notificationRepo
       ],
       child: MultiBlocProvider(
         providers: [
@@ -197,13 +186,6 @@ class MainApp extends StatelessWidget {
           ),
           BlocProvider(
             create: (context) => ProductsFilterBloc(),
-          ),
-          BlocProvider(
-            create: (context) => HamperBloc(context.read<ShoppingRepository>())
-              ..add(FetchHampers()),
-            create: (context) => HamperBloc(
-              context.read<ShoppingRepository>(),
-            ),
           ),
         ],
 
